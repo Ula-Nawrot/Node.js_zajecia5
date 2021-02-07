1f316d70c67ccc0d5c01e262ccbb7c0d0d2acbe3
UlaNawrot <ula.nawrot@gmail.com>
2021-02-07 09:43:31 +0100

    wykorzystanie biblioteki Express

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const http = require('http');
-const handler = (request, response) => {
-    console.log('sample message');
-    response.end("Hello World");
-
-}
-const server = http.createServer(handler);
+const express = require("express"); //nie korzystam już z http!!
 const port = 3000;
+const app = express();
 
-server.listen(port, (err) => {
+
+app.get('/', function (req, res) {
+    res.send("Hello World");
+})
+//jesli jestesmy na podstronie to wpisujemy app.get('/about')
+app.get('/about', function (req, res){
+    res.send("O mnie")
+})
+
+
+app.listen(port, (err) => {
     if(err){return console.log('coś poszło nie tak..:', err);}
     console.log(`serwer działa na porcie ${port}`);
 })
