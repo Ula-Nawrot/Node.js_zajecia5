09e045d465c12f58f93aaaa23f4fe11f22d970db
UlaNawrot <ula.nawrot@gmail.com>
2021-02-07 09:51:24 +0100

    uzycie hbs

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express"); //nie korzystam już z http!!
 const port = 3000;
 const app = express();
 
-
+app.set("view engine", 'hbs')
+//gdy uzytkownik wchodzi na stronę 
 app.get('/', function (req, res) {
-    res.send("Hello World");
-})
+    res.render('index')
+}) 
 //jesli jestesmy na podstronie to wpisujemy app.get('/about')
 app.get('/about', function (req, res){
     res.send("O mnie")
